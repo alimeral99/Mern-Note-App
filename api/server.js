3ee5ad495b991e2463d89bb4ef4cd94d751f19dc
Ali Meral <meralmuhammet544@gmail.com>
2024-06-29 16:05:22 +0300

    connect to database

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,5 +8,14 @@ require("dotenv").config();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5000, console.log("Server running on port 5000"));
