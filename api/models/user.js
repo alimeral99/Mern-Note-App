c199ef3d0c44b8f70878ad2a5f449960bb9a9bcd
Ali Meral <meralmuhammet544@gmail.com>
2024-06-29 16:12:51 +0300

    create user model

---
+++ api/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
