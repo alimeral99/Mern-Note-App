10814ba2e80d50d9cb94ae3c1d9dca1642fd3664
Ali Meral <meralmuhammet544@gmail.com>
2024-06-30 21:10:43 +0300

    Set navbar according to user

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="header">
       <Link className="header__logo " to={"/"}>
@@ -10,13 +13,25 @@ function Header() {
       </Link>
 
       <div className="header__right">
-        <Link to="/login" className="links">
-          Login
-        </Link>
-
-        <Link to="/signup" className="links">
-          Sign Up
-        </Link>
+        {currentUser ? (
+          <>
+            <Link to="/profile" className="links">
+              Profile
+            </Link>
+            <a href="/login" className="links">
+              Logout
+            </a>
+          </>
+        ) : (
+          <>
+            <Link className="links" to="/login">
+              Login
+            </Link>
+            <Link className="links" to="/signUp">
+              SignUp
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
