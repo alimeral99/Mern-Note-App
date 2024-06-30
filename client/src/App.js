4624e84e973343e5db66c3285d8c9fb4712d4282
Ali Meral <meralmuhammet544@gmail.com>
2024-06-30 19:58:52 +0300

    Create component for profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header/Header";
 import Signup from "./Signup/Signup";
 import Login from "./Login/Login";
+import Profile from "./Profile/Profile";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<h1>hello world</h1>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/Profile/Profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Profile.css";
+
+function Profile() {
+  return <div>This Page is Profile</div>;
+}
+
+export default Profile;
