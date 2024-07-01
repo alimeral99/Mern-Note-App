5947c5f1ea133bc39bc0efcf0ce307fa2bb50cfa
Ali Meral <meralmuhammet544@gmail.com>
2024-07-01 15:06:33 +0300

    Authenticate middeware

---
+++ api/utils/authMiddleWare.js
@@ -0,0 +1,32 @@
+const User = require("../models/user");
+const { errorHandler } = require("./error");
+
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      return next(errorHandler(401, "Not authorized"));
+    }
+  }
+};
+
+module.exports = {
+  authenticateToken,
+};
