62185761b7069e67fdeee0b1e0711ba2bc3511a9
Ali Meral <meralmuhammet544@gmail.com>
2024-07-29 21:24:23 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://mern-note-app-1.onrender.com",
     credentials: true,
     methods: "PATCH,DELETE,POST,GET",
   })
